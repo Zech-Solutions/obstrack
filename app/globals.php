57359ea23de47f80d4e46ca5040e968af89946a9
Eduard Rino Carton <eduard.carton@exocoder.io>
2025-01-24 11:18:31 +0800

    login

--- app/globals.php
+++ app/globals.php
@@ -9,5 +9,14 @@ define('ROUTE_URL', 'obstrack/public');
 define('URL', 'http://localhost/obstrack/');
 define('URL_PUBLIC', URL . 'public');
 
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'u844358091_obstrack_user');
+// define('DB_PASSWORD', '0b$tr4cK2025!');
+// define('DB_NAME', 'u844358091_obstrack_db');
+
+// define('ROUTE_URL', 'obstrack/public');
+// define('URL', 'https://bacoders.com/obstrack/');
+// define('URL_PUBLIC', URL . 'public');
+
 define('SYSTEM', 'obstrack');
 define('SYSTEM_NAME', 'OBSTRACK');
--- index.php
+++ index.php
@@ -21,204 +21,164 @@ unset($_SESSION[SYSTEM]['success']);
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup & Login Form</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?= URL_PUBLIC ?>/vendor/bootstrap/bootstrap.min.css">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Obstrack - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <!-- <link href="../../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link rel="stylesheet" href="<?= URL_PUBLIC ?>/css/sb-admin-2.min.css">
     <style>
-        .form-container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            min-height: 100vh;
-            background-color: #f7f7f7;
+        :root {
+            --login-logo: url("<?= URL_PUBLIC ?>/images/login_logo.png");
         }
 
-        .form-box {
-            background: white;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            max-width: 800px;
-            width: 100%;
+        .bg-gradient-etally {
+            background-color: #056f39;
+            background-image: linear-gradient(180deg, #056f39 1%, #3faf53 58%);
+            background-size: cover;
+            background-repeat: no-repeat;
         }
 
-        .image-container {
-            flex: 1;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 20px;
-        }
-
-        .image-container img {
-            max-width: 100%;
-            height: auto;
-        }
-
-        .form-content {
-            flex: 1;
-            padding: 20px;
-            width: 100%;
-        }
-
-        .form-group {
-            margin-bottom: 15px;
-        }
-
-        .form-content h2 {
-            margin-bottom: 20px;
-        }
-
-        .form-footer {
-            text-align: center;
-            margin-top: 20px;
-            font-size: 14px;
-            color: #777;
-        }
-
-        .btn-social {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-top: 10px;
-        }
-
-        .btn-social i {
-            margin-right: 10px;
-        }
-
-        @media (max-width: 768px) {
-            .form-box {
-                flex-direction: column;
-                align-items: center;
-                text-align: center;
-            }
-
-            .image-container,
-            .form-content {
-                width: 100%;
-            }
-
-            .form-content {
-                padding: 20px 0;
-            }
+        .bg-login-image-etally {
+            background-image: var(--login-logo);
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
         }
     </style>
 </head>
 
-<body>
+<body class="bg-gradient-etally">
 
-    <div class="container form-container">
-        <div class="form-box">
-            <div class="image-container" id="image-container">
-                <!-- Replace the src with your traffic incidents image URL -->
-                <img src="https://via.placeholder.com/100x100.png?text=Traffic+Incidents" alt="Traffic Incidents Image">
-            </div>
-            <div class="form-content">
-                <form id="login-form" action="<?= URL_PUBLIC ?>/login" method="POST">
-                    <h2>Login</h2>
-                    <div class="form-group">
-                        <label for="login-username">Username</label>
-                        <input type="text" class="form-control" id="login-username" placeholder="Enter username" name="username" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="login-password">Password</label>
-                        <input type="password" class="form-control" id="login-password" placeholder="Password" name="password" required>
-                    </div>
-                    <div class="form-group">
-                        <?=$badge_error?>
-                        <?=$badge_success?>
-                    </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="login-remember">
-                        <label class="form-check-label" for="login-remember">Remember me</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                    <p class="mt-3">Don't have an account? <a href="#" onclick="toggleForm()">Sign Up</a></p>
-                    <p><a href="#">Forgot password?</a></p>
-                </form>
+    <div class="container mt-5">
 
-                <form action="<?= URL_PUBLIC ?>/register" method="POST" id="signup-form" style="display: none;">
-                    <h2>Sign Up</h2>
-                    <div class="form-row">
-                        <div class="form-group col-md-4">
-                            <label for="signup-firstname">First Name</label>
-                            <input type="text" class="form-control" id="signup-firstname" placeholder="Enter first name" name="first_name" required>
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-5 d-none d-lg-block bg-login-image-etally img-container" id="image-container"></div>
+                            <div class="col-lg-7" id="form-container">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Bacolod Road Obstruction Incident Report Management Application</h1>
+                                    </div>
+                                    <form id="login-form" class="user" action="<?= URL_PUBLIC ?>/login" method="POST">
+                                        <div class="form-group">
+                                            <input name="username" type="username" class="form-control form-control-user"
+                                                id="username" aria-describedby="emailHelp"
+                                                placeholder="Username" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <input name="password" type="password" class="form-control form-control-user"
+                                                id="password" placeholder="Password" required>
+                                        </div>
+                                        <div class="bd-example login-response">
+                                            <?= $badge_error ?>
+                                            <?= $badge_success ?>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+                                        <p class="mt-3">Don't have an account? <a href="#" onclick="toggleForm()">Sign Up</a></p>
+                                    </form>
+                                    <form action="<?= URL_PUBLIC ?>/register" method="POST" id="signup-form" style="display: none;">
+                                        <div class="form-row">
+                                            <div class="form-group col-md-4">
+                                                <label for="signup-firstname">First Name</label>
+                                                <input type="text" class="form-control form-control-user" id="signup-firstname" placeholder="Enter first name" name="first_name" required>
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="signup-middlename">Middle Name</label>
+                                                <input type="text" class="form-control" id="signup-middlename" placeholder="Enter middle name" name="middle_name">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="signup-lastname">Last Name</label>
+                                                <input type="text" class="form-control" id="signup-lastname" placeholder="Enter last name" name="last_name" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="form-group col-md-6">
+                                                <label for="signup-birthdate">Birthdate</label>
+                                                <input type="date" class="form-control" id="signup-birthdate" name="dob" max="<?= date('Y-m-d') ?>" required>
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="signup-gender">Gender</label>
+                                                <select class="form-control" id="signup-gender" name="gender" required>
+                                                    <option value="" disabled selected>Select gender</option>
+                                                    <option value="M">Male</option>
+                                                    <option value="F">Female</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="signup-address">Address</label>
+                                            <textarea class="form-control" id="signup-address" placeholder="Enter address" rows="2" name="address" required></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="signup-username">Username</label>
+                                            <input type="text" class="form-control" id="signup-username" placeholder="Enter username" name="username" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="signup-password">Password</label>
+                                            <input type="password" class="form-control" id="signup-password" placeholder="Password" name="password" required>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Sign Up
+                                        </button>
+                                        <p class="mt-3">Already have an account? <a href="#" onclick="toggleForm()">Log In</a></p>
+                                    </form>
+                                    <hr>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group col-md-4">
-                            <label for="signup-middlename">Middle Name</label>
-                            <input type="text" class="form-control" id="signup-middlename" placeholder="Enter middle name" name="middle_name">
+                        <div class="container my-auto">
+                            <div class="copyright text-center my-auto">
+                                <span>Bacolod Road Obstruction Incident Report Management Application &copy; 2023</span>
+                            </div>
                         </div>
-                        <div class="form-group col-md-4">
-                            <label for="signup-lastname">Last Name</label>
-                            <input type="text" class="form-control" id="signup-lastname" placeholder="Enter last name" name="last_name" required>
-                        </div>
+                        <br>
                     </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="signup-birthdate">Birthdate</label>
-                            <input type="date" class="form-control" id="signup-birthdate" name="dob" max="<?=date('Y-m-d')?>" required>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label for="signup-gender">Gender</label>
-                            <select class="form-control" id="signup-gender" name="gender" required>
-                                <option value="" disabled selected>Select gender</option>
-                                <option value="M">Male</option>
-                                <option value="F">Female</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="signup-address">Address</label>
-                        <textarea class="form-control" id="signup-address" placeholder="Enter address" rows="2" name="address" required></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="signup-username">Username</label>
-                        <input type="text" class="form-control" id="signup-username" placeholder="Enter username" name="username" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="signup-password">Password</label>
-                        <input type="password" class="form-control" id="signup-password" placeholder="Password" name="password" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
-                    <p class="mt-3">Already have an account? <a href="#" onclick="toggleForm()">Log In</a></p>
-                </form>
-            </div>
-            <div class="form-footer">
-                <p>Bacolod Road Obstruction Incident Report Management Application</p>
+                </div>
             </div>
         </div>
     </div>
-
-    <!-- Font Awesome for social icons -->
-    <!-- <script src="https://kit.fontawesome.com/a076d05399.js"></script> -->
-    <!-- Bootstrap JS and jQuery -->
-    <script src="<?= URL_PUBLIC ?>/vendor/jquery/jquery.min.js"></script>
-    <script src="<?= URL_PUBLIC ?>/vendor/popper/popper.min.js"></script>
-    <script src="<?= URL_PUBLIC ?>/vendor/bootstrap/bootstrap.min.js"></script>
-
     <script>
         function toggleForm() {
             const loginForm = document.getElementById('login-form');
             const signupForm = document.getElementById('signup-form');
             const imageContainer = document.getElementById('image-container');
+            const formContainer = document.getElementById('form-container');
             if (loginForm.style.display === 'none') {
                 loginForm.style.display = 'block';
                 signupForm.style.display = 'none';
-                imageContainer.style.display = 'flex';
+                imageContainer.style.display = 'block';
+                formContainer.classList.remove('col-lg-12');
+                formContainer.classList.add('col-lg-7');
             } else {
                 loginForm.style.display = 'none';
                 signupForm.style.display = 'block';
                 imageContainer.style.display = 'none';
+                formContainer.classList.remove('col-lg-7');
+                formContainer.classList.add('col-lg-12');
             }
         }
     </script>
-
 </body>
 
 </html>
