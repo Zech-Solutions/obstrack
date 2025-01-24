0b47211d7a8297f0ab21c078e880d45f008f8e15
Eduard Rino Carton <eduard.carton@exocoder.io>
2025-01-24 11:19:18 +0800

    Update globals.php

--- app/globals.php
+++ app/globals.php
@@ -1,22 +1,22 @@
 <?php
 // Define global variables
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'obstrack_db');
-
-define('ROUTE_URL', 'obstrack/public');
-define('URL', 'http://localhost/obstrack/');
-define('URL_PUBLIC', URL . 'public');
-
 // define('DB_HOST', 'localhost');
-// define('DB_USER', 'u844358091_obstrack_user');
-// define('DB_PASSWORD', '0b$tr4cK2025!');
-// define('DB_NAME', 'u844358091_obstrack_db');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'obstrack_db');
 
 // define('ROUTE_URL', 'obstrack/public');
-// define('URL', 'https://bacoders.com/obstrack/');
+// define('URL', 'http://localhost/obstrack/');
 // define('URL_PUBLIC', URL . 'public');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'u844358091_obstrack_user');
+define('DB_PASSWORD', '0b$tr4cK2025!');
+define('DB_NAME', 'u844358091_obstrack_db');
+
+define('ROUTE_URL', 'obstrack/public');
+define('URL', 'https://bacoders.com/obstrack/');
+define('URL_PUBLIC', URL . 'public');
 
 define('SYSTEM', 'obstrack');
 define('SYSTEM_NAME', 'OBSTRACK');
